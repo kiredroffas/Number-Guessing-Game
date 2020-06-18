37c95dc68e952d9b321d7be50ba41265bc14d36b
Erik <erik.safford@wsu.edu>
2020-06-17 23:27:03 -0700

    Created hello world

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry point method
+        static void Main(string[] args)
+        {
+            // Start here
+            Console.WriteLine("Hello world");
+        }
+    }
+}
